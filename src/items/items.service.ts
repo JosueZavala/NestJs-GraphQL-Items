73c39823d04acb9635426d9d52f80ea404b279c9
Josue Zavala <josue.zavala123@gmail.com>
2025-01-24 15:30:18 -0600

    feat(items): Adding SearchArgs to args items and changing consult by queryBuilder

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,10 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
 import { Item } from './entities/item.entity';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
-import { PaginationArgs } from '../common/dto/args/pagination.args';
+import { PaginationArgs, SearchArgs } from '../common/dto/args';
 
 @Injectable()
 export class ItemsService {
@@ -18,19 +18,38 @@ export class ItemsService {
     return await this.itemsRepository.save(newItem);
   }
 
-  async findAll(user: User, paginationArgs: PaginationArgs): Promise<Item[]> {
+  async findAll(
+    user: User,
+    paginationArgs: PaginationArgs,
+    searchArgs: SearchArgs,
+  ): Promise<Item[]> {
     const { offset, limit } = paginationArgs;
+    const { search } = searchArgs;
 
-    // TODO: filtrar
-    return this.itemsRepository.find({
+    const queryBuilder = this.itemsRepository
+      .createQueryBuilder()
+      .take(limit)
+      .skip(offset)
+      .where(`"userId" = :userId`, { userId: user.id });
+
+    if (search) {
+      queryBuilder.andWhere('LOWER(name) like :name', {
+        name: `%${search.toLocaleLowerCase()}%`,
+      });
+    }
+
+    return queryBuilder.getMany();
+
+    /*     return this.itemsRepository.find({
       take: limit,
       skip: offset,
       where: {
         user: {
           id: user.id,
         },
+        name: Like(`${search}`), // * select * from items where name like '%search%'
       },
-    });
+    }); */
   }
 
   async findOne(id: string, user: User): Promise<Item> {
