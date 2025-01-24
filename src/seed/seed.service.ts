31148645514e674ecc7cbeb7c84649e33390844f
Josue Zavala <josue.zavala123@gmail.com>
2025-01-24 13:06:52 -0600

    feat(seed): Adding data and filling users and items in DB

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,6 +4,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Item } from 'src/items/entities/item.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import { SEED_ITEMS, SEED_USERS } from './data/seed-data';
+import { UsersService } from '../users/users.service';
+import { ItemsService } from 'src/items/items.service';
 
 @Injectable()
 export class SeedService {
@@ -15,6 +18,8 @@ export class SeedService {
     private readonly itemsRepository: Repository<Item>,
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    private readonly usersService: UsersService,
+    private readonly itemsService: ItemsService,
   ) {
     this.isProd = configService.get('STATE') === 'prod';
   }
@@ -28,7 +33,9 @@ export class SeedService {
     // ? Limpiar la base de datos (Borrar todo)
     await this.deleteDatabase();
     // ? Crear Usuarios
+    const user = await this.loadUsers();
     // ? Crear Items
+    await this.loadItems(user);
 
     return true;
   }
@@ -46,5 +53,23 @@ export class SeedService {
       .delete()
       .where({})
       .execute();
+  }
+
+  async loadUsers(): Promise<User> {
+    const users = [];
+    for (const user of SEED_USERS) {
+      users.push(await this.usersService.create(user));
+    }
+
+    return users[0];
+  }
+
+  async loadItems(user: User): Promise<void> {
+    const itemPromises = [];
+    for (const item of SEED_ITEMS) {
+      itemPromises.push(this.itemsService.create(item, user));
+    }
+
+    await Promise.all(itemPromises);
   }
 }
