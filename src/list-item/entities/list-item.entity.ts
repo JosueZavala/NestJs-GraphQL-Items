b71c7f760ccfd2edafa9d61b502f582a70899de8
Josue Zavala <josue.zavala123@gmail.com>
2025-01-31 13:35:28 -0600

    feat(list-item): Creating the resource and Entity

---
+++ src/list-item/entities/list-item.entity.ts
@@ -0,0 +1,25 @@
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Item } from 'src/items/entities/item.entity';
+import { List } from 'src/lists/entities/list.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('listItems')
+@ObjectType()
+export class ListItem {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @Column({ type: 'numeric' })
+  @Field(() => Number)
+  quantity: number;
+
+  @Column({ type: 'boolean' })
+  @Field(() => Boolean)
+  completed: boolean;
+
+  // Relaciones
+  /* list: List;
+
+  item: Item; */
+}
---
+++ src/list-item/list-item.module.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+
+import { ListItemService } from './list-item.service';
+import { ListItemResolver } from './list-item.resolver';
+
+import { ListItem } from './entities/list-item.entity';
+
+@Module({
+  providers: [ListItemResolver, ListItemService],
+  imports: [TypeOrmModule.forFeature([ListItem])],
+  exports: [ListItemService, TypeOrmModule],
+})
+export class ListItemModule {}
